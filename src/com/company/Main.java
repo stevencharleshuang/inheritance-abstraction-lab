28504c020174b9f8bd9e44a91a45d54deafc5b36
Steven Huang <steven.charles.huang@gmail.com>
2019-10-03 18:46:51 -0400

    added second grade

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        You'll need to design a program that manages a school.
-//        The school has classrooms, and the classrooms have teachers and students assigned to them.
-//        Each classroom contains students in a single grade (i.e., first, second, third, and so on).
-//
-//        You decide what the actions and properties of each class will be.
-//
-//        Requirements
-//        At least one abstract class.
-//        At least one interface.
-//        At least three subclasses.
-//        Each subclass must be unique from the others.
-//        Bonus: Add extra classes, subclasses, or interfaces.
+        // You'll need to design a program that manages a school.
+        // The school has classrooms, and the classrooms have teachers and students assigned to them.
+        // Each classroom contains students in a single grade (i.e., first, second, third, and so on).
+
+        // You decide what the actions and properties of each class will be.
+
+        // Requirements
+        // At least one abstract class.
+        // At least one interface.
+        // At least three subclasses.
+        // Each subclass must be unique from the others.
+        // Bonus: Add extra classes, subclasses, or interfaces.
 
         // Instantiate School
         School school = new School("P.S. 36");
@@ -26,5 +26,14 @@ public class Main {
         Teacher firstGradeTeacher = new Teacher("P.S. 36", 1, "Mrs. Caputo", 45, 1, "English");
         Student firstGraderSteve = new Student("P.S. 36", 1, "Steve", 6, 1, "Social Studies");
         Student firstGraderKat = new Student("P.S. 36", 1, "Kat", 6, 1, "English");
+        Student firstGraderMarco = new Student("P.S. 36", 1, "Marco", 5, 1, "Math");
+
+        Teacher secondGradeTeacher = new Teacher("P.S. 36", 2, "Mrs. Caputo", 45, 2, "Math");
+        Student secondGraderStacey = new Student("P.S. 36", 2, "Stacey", 6, 2, "Science");
+        Student secondGraderPaul = new Student("P.S. 36", 2, "Paul", 6, 2, "Art");
+        Student secondGraderPolo = new Student("P.S. 36", 2, "Polo", 5, 2, "Music");
+
+
+
     }
 }
